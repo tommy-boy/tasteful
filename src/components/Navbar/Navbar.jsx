67c30d6bedb3c28d1b7f5e0ad40168aa5a94649e
unknown <tom.techcrunch@gmail.com>
2019-11-13 12:29:11 -0700

    add icons

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,13 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import { withStyles } from "@material-ui/styles";
-import InfoTwoTone from "@material-ui/icons/InfoTwoTone";
+import { IconContext } from "react-icons";
+import {
+  MdInfoOutline,
+  MdChatBubbleOutline,
+  MdPersonOutline
+} from "react-icons/md";
+import { IoIosLogIn } from "react-icons/io";
 import navBarStyles from "./styles";
 
 const Navbar = props => {
@@ -10,26 +16,45 @@ const Navbar = props => {
   return (
     <Fragment>
       <nav className={classes.navbar}>
-        <Link className={classes.navitems} to={"/about"}>
-          <InfoTwoTone />
-          About
-        </Link>
-        <Link className={classes.navitems} to={"/contact"}>
-          Contact
-        </Link>
-        {/* {auth.user ? ( */}
-        <Fragment>
-          {/* <Link to="/account">Account ({auth.user.email})</Link> */}
-          <Link className={classes.navitems} to="/account">
-            Account
+        <IconContext.Provider
+          value={{
+            color: "white",
+            size: "24px",
+            style: { verticalAlign: "middle", marginRight: "4px" }
+          }}
+        >
+          {/* {auth.user ? ( */}
+          <Fragment>
+            {/* <Link to="/account">Account ({auth.user.email})</Link> */}
+            <Link className={classes.navitems} to="/account">
+              <MdPersonOutline />
+              Account
+            </Link>
+            {/* <Button onClick={() => auth.signout()}>Signout</Button> */}
+          </Fragment>
+          {/* ) : ( */}
+          <Link className={classes.navitems} to={"/contact"}>
+            <MdChatBubbleOutline />
+            Chat
           </Link>
-          {/* <Button onClick={() => auth.signout()}>Signout</Button> */}
-        </Fragment>
-        {/* ) : ( */}
-        <Link className={classes.navitems} to="/signin">
-          Signin
-        </Link>
-        {/* )} */}
+          <Link className={classes.navitems} to={"/info"}>
+            <MdInfoOutline />
+            Info
+          </Link>
+        </IconContext.Provider>
+        <IconContext.Provider
+          value={{
+            color: "white",
+            size: "24px",
+            style: { verticalAlign: "middle", marginLeft: "4px" }
+          }}
+        >
+          <Link className={classes.navitems} to="/signin">
+            Signin
+            <IoIosLogIn />
+          </Link>
+          {/* )} */}
+        </IconContext.Provider>
       </nav>
     </Fragment>
   );
