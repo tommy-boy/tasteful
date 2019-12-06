5c6fd696db291cf4630ef2e426d15c218f67e42e
unknown <tom.techcrunch@gmail.com>
2019-12-05 21:09:41 -0700

    body parser

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
-//const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 const path = require("path");
 const port = process.env.PORT || 8080;
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
